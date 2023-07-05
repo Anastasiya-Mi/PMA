83d45f46eda53d6fa9f8d18a7641d1d6e3e90a9e
“Anastasiya <anm6468276@gmail.com>
2023-07-05 23:05:14 +0300

    feat:set up the environment,create server,set up app launch, create login/register routes

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const authRoutes = require('./routes/auth')
+const app = express();
+//http://localhost:5000/api/auth/
+//http://localhost:5000/api/auth/login
+app.use('/api/auth',authRoutes);
+
+module.exports = app;
