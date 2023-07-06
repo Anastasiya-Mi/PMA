d7fdcadcfa2ee704a04f94c14dcbfec7c21b63c6
“Anastasiya <anm6468276@gmail.com>
2023-07-06 23:10:42 +0300

    feat: add passport

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const passport = require('passport');
 const cors = require('cors');
 const morgan = require('morgan');
+
 const authRoutes = require('./routes/auth');
 const analyticsRoutes = require('./routes/analytics');
 const categoryRoutes = require('./routes/category');
@@ -15,7 +17,8 @@ mongoose.connect(keys.mongoURI)
 .then(()=> console.log('gg'))
 .catch(error => console.log(error))
 
-
+app.use(passport.initialize())
+require('./middleware/passport')(passport)
 
 
 
@@ -30,6 +33,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json())
 app.use('/api/auth',authRoutes);
 app.use('/api/analytics',analyticsRoutes);
+//http://localhost:5000/api/category
 app.use('/api/category',categoryRoutes);
 app.use('/api/order',orderRoutes);
 app.use('/api/position',positionRoutes);
